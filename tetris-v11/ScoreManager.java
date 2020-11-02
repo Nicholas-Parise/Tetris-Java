75379c6dbac3c1bdc232a1bd9140958c9db77f85
Nicholas-Parise <54895782+Nicholas-Parise@users.noreply.github.com>
2020-11-02 09:51:35 -0500

    tetris-v11

---
+++ tetris-v11/ScoreManager.java
@@ -0,0 +1,18 @@
+public class ScoreManager {
+
+	public static int score = 0;
+    public static int Highscore = 0;
+
+
+public static void saveScore(){
+
+}
+
+
+public static void GetScore(){
+
+}
+
+
+
+}
