bbea0ad10a3cb03500b4a8dd11856841897de25d
Nicholas-Parise <54895782+Nicholas-Parise@users.noreply.github.com>
2024-01-16 21:43:22 -0500

    V4

--- Tetris/Main.java
+++ Tetris/Main.java
@@ -1,215 +1,142 @@
 import java.awt.*;
-import java.awt.geom.Line2D;
 import javax.swing.*;
+import java.awt.geom.Line2D;
 import java.util.Scanner;
+import java.util.concurrent.TimeUnit;
 
-public class Main extends Canvas{
-	  
+// Due Nov 11
 
-	
-	 public static int[][] GameMatrix = new int[20][10];
-	
-	
-	 
+public class Main extends JPanel {
 
-	  public static void main(String[] args) {
-		
-		  
-		  JFrame frame = new JFrame("My Drawing");
-	        Canvas canvas = new Main();
-	        canvas.setSize(500, 500);
-	        frame.add(canvas);
-	        frame.pack();
-	        frame.setVisible(true);
-		  
-	        frame.repaint();
-	        
-	        String Input;
-		  
-		  for(int i = 0; i<20; i++) {
-		        for(int j = 0; j<10; j++) {
-		        	GameMatrix[i][j] = 0;
-		        }}
-		  
-		  		//	  [Y][X]
-		 
-	
-				  Blocks.SpriteShape = Blocks.LShape;
-				  
-				  Blocks.CurrentShape = 0;
-						  
-				Blocks.CurrentSymbol = 8;
-		  
-				System.out.println();
-				Blocks.BlockHandler("U");
+	public static void main(String[] args) {
 
-		  for(int i = 0; i <4;i++) {
-			  for(int j = 0; j <4; j++) {
-			  
-				 if(Blocks.SpriteShape[i][j]!= 0) {
-					 
-					 GameMatrix[i+Blocks.offsetY][j+Blocks.offsetX] = Blocks.SpriteShape[i][j];
-				 }
-			  }
-		  }
-		  
-		  for(int i = 0; i<20; i++) {
-		        for(int j = 0; j<10; j++) {
-		        	System.out.print(GameMatrix[i][j]+" ");
-		        }
-		  System.out.println();      
-		  }
-		  
-		  frame.repaint();
-		  
-		  Scanner sc = new Scanner(System.in);
-		  System.out.print("asdasdasdw ");
-		  Input = sc.next();
+		JFrame frame = new JFrame();
+		frame.getContentPane().add(new Main());
 
-			System.out.println();
-			Blocks.BlockHandler("R");
+		frame.setTitle("Tetris");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		frame.setBackground(Color.red);
 
-	  for(int i = 0; i <4;i++) {
-		  for(int j = 0; j <4; j++) {
-		  
-			 if(Blocks.SpriteShape[i][j]!= 0) {
-				 
-				 GameMatrix[i+Blocks.offsetY][j+Blocks.offsetX] = Blocks.SpriteShape[i][j];
-			 }
-		  }
-	  }
-	  
-	  for(int i = 0; i<20; i++) {
-	        for(int j = 0; j<10; j++) {
-	        	System.out.print(GameMatrix[i][j]+" ");
-	        }
-	  System.out.println();      
-	  }
-	  
-	  frame.repaint();
-	  
-	  System.out.print("asdasdasdw ");
-	 Input = sc.next();
-	  
+		frame.setLocationRelativeTo(null);
+		frame.setSize(500, 600);
+		frame.setVisible(true);
+
+		KeyBoard keyboard = new KeyBoard();
+
+		keyboard.addKeyListener(keyboard);
+		keyboard.setFocusable(true);
+		frame.add(keyboard);
+		frame.getKeyListeners();
+
+		frame.validate();
+		frame.repaint();
+
+		Blocks.ResetMatrix();
+
+		// [Y][X]
+
+
+		Blocks.MakeBlockQueue();
+		Blocks.NextBlock();
+
 		System.out.println();
-		Blocks.BlockHandler("D");
+		Blocks.BlockHandler(0);
 
-for(int i = 0; i <4;i++) {
-	  for(int j = 0; j <4; j++) {
-	  
-		 if(Blocks.SpriteShape[i][j]!= 0) {
-			 
-			 GameMatrix[i+Blocks.offsetY][j+Blocks.offsetX] = Blocks.SpriteShape[i][j];
-		 }
-	  }
-}
+		Blocks.UpdateMatrix();
 
-for(int i = 0; i<20; i++) {
-      for(int j = 0; j<10; j++) {
-      	System.out.print(GameMatrix[i][j]+" ");
-      }
-System.out.println();      
-}
-
-System.out.print("asdasdasdw ");
-Input = sc.next();
-
-frame.repaint();
-
-System.out.println();
-Blocks.BlockHandler("L");
-
-for(int i = 0; i <4;i++) {
-for(int j = 0; j <4; j++) {
-
- if(Blocks.SpriteShape[i][j]!= 0) {
-	 
-	 GameMatrix[i+Blocks.offsetY][j+Blocks.offsetX] = Blocks.SpriteShape[i][j];
- }
-}
-}
-
-for(int i = 0; i<20; i++) {
-for(int j = 0; j<10; j++) {
-	System.out.print(GameMatrix[i][j]+" ");
-}
-System.out.println();      
-}
+		// Blocks.PrintMatrix();
 
 
-System.out.print("asdasdasdw ");
-Input = sc.next();
+		Boolean Test = false;
 
-frame.repaint();
 
-System.out.println();
-Blocks.BlockHandler("U");
+		Blocks.MakeBlockQueue();
 
-for(int i = 0; i <4;i++) {
-for(int j = 0; j <4; j++) {
+		try {
+			TimeUnit.SECONDS.sleep(2);
+			Test = true;
+		} catch (Exception e) {
 
- if(Blocks.SpriteShape[i][j]!= 0) {
-	 
-	 GameMatrix[i+Blocks.offsetY][j+Blocks.offsetX] = Blocks.SpriteShape[i][j];
- }
-}
-}
+		}
 
-for(int i = 0; i<20; i++) {
-for(int j = 0; j<10; j++) {
-	System.out.print(GameMatrix[i][j]+" ");
-}
-System.out.println();      
-}
+		do {
+
+			Blocks.Gravity();
+
+			// Blocks.PrintMatrix();
+
+
+		if(Blocks.WhereFloor()){
+
+			Blocks.NextBlock();
+		}
 
 
 
-frame.repaint();
+			try {
+
+				frame.validate();
+				frame.repaint();
+
+				TimeUnit.MILLISECONDS.sleep(100);
+			} catch (Exception e) {
+				System.out.println("Failed To draw");
+			}
+
+		} while (Test);
 
 	}
-	
 
-	  public void paint(Graphics g) {
-		  
-		  
-		   Color colors[] = {new Color(0, 0, 0), new Color(255, 50, 50),
-	                new Color(50, 255, 50), new Color(50, 50, 255),
-	                new Color(225, 225, 50), new Color(225, 50, 225),
-	                new Color(50, 225, 225), new Color(250, 170, 0),new Color(255,255,255)
-	        };
-		   
-		  //black0,red1,green2,blue3,yellow4,purple5,cyan6,orange7,white8
-		  
-		  
-		  for(int i = 0; i<20; i++) {
-		        for(int j = 0; j<10; j++) {
-		  
-		  int x = 20*j+j+100;
-		  int y = 20*i+i;
-		  int width = 20;
-		  int height = 20;
-		  
-		  var color = colors[0];
-		  
-		  if(GameMatrix[i][j]!=0) {
+	public void paint(Graphics g) {
+		System.out.println("y");
+		// colors
 
-			  color = colors[6];
-		  }else {
-			  
-			  color = colors[8];			  
-		  }
+		Color colors[] = { new Color(0, 0, 0), new Color(255, 50, 50), new Color(50, 255, 50), new Color(50, 50, 255),
+				new Color(225, 225, 50), new Color(225, 50, 225), new Color(50, 225, 225), new Color(250, 170, 0),
+				new Color(255, 255, 255) };
 
-		  
-	        g.setColor(color);
-	        g.fillRect(x + 1, y + 1, width - 2, height - 2);
+		// black0,red1,green2,blue3,yellow4,purple5,cyan6,orange7,white8
 
-		        }}
-		  
-	  }
-	  
-	  
-	  
-	  
-	
-	
+		for (int i = 0; i < 20; i++) {
+			for (int j = 0; j < 10; j++) {
+
+				int x = 20 * j + j + 100;
+				int y = 20 * i + i;
+				int width = 20;
+				int height = 20;
+
+				var color = colors[0];
+
+				if (Blocks.GameMatrix[i][j] != 0) {
+
+					if (Blocks.GameMatrix[i][j] == 1) {
+
+						color = colors[1];
+					} else if (Blocks.GameMatrix[i][j] == 2) {
+						color = colors[2];
+					} else if (Blocks.GameMatrix[i][j] == 3) {
+						color = colors[3];
+					} else if (Blocks.GameMatrix[i][j] == 4) {
+						color = colors[4];
+					} else if (Blocks.GameMatrix[i][j] == 5) {
+						color = colors[5];
+					} else if (Blocks.GameMatrix[i][j] == 6) {
+						color = colors[6];
+					} else if (Blocks.GameMatrix[i][j] == 7) {
+						color = colors[7];
+					}
+
+				} else {
+
+					color = colors[8];
+				}
+
+				g.setColor(color);
+				g.fillRect(x + 1, y + 1, width - 2, height - 2);
+
+			}
+		}
+
+	}
+
 }
